7d21feec1d349fb65214e6127ac00daff656a923
Miranda Sim <95054232+mirandajsim@users.noreply.github.com>
2021-11-25 23:59:34 +0000

    Play/pause functionality and boss key added.
    Play/pause button to pause and resume the game. Boss key launches when 'tab' is pressed, opening a screenshot of a Word document.

--- pong.py
+++ pong.py
@@ -1,24 +1,45 @@
-from tkinter import Tk, Canvas, PhotoImage
+from tkinter import *
 import random
 
 width = 1280 # width of world
 height = 720 # height of world
 
+def bosskey(event):
+    global bossflag
+    pausegame()
+    if bossflag is True:
+        bossflag = False
+        bosskeywindow.withdraw()
+        window.deiconify()
+    else:
+        bossflag = True
+        bosskeywindow.deiconify()
+        window.withdraw()
+
 def leftkey(event):
-	global direction
-	direction = 'left'
-	(x, y, a, b) = canvas.coords(platform)
-	if a > 0:
-		edge = min(x, 10)
-		canvas.move(platform, -edge, 0)
+	global direction, pause
+	if pause is False:
+		direction = 'left'
+		(x, y, a, b) = canvas.coords(platform)
+		if a > 0:
+			edge = min(x, 10)
+			canvas.move(platform, -edge, 0)
 
 def rightkey(event):
-	global direction
-	direction = 'right'
-	(x, y, a, b) = canvas.coords(platform)
-	if a < width:
-		edge = min(width - a, 10)
-		canvas.move(platform, edge, 0)
+	global direction, pause
+	if pause is False:
+		direction = 'right'
+		(x, y, a, b) = canvas.coords(platform)
+		if a < width:
+			edge = min(width - a, 10)
+			canvas.move(platform, edge, 0)
+
+def pausegame():
+    global pause
+    if pause is True:
+        pause = False
+    else:
+        pause = True
 
 def overlapping(a, b):
 	if a[0] < b[2] and a[2] > b[0] and a[1] < b[3] and a[3] > b[1]:
@@ -27,31 +48,52 @@ def overlapping(a, b):
 
 def moveball():
 	canvas.pack()
-	global speedx, speedy, score
-	(x, y, a, b) = canvas.coords(ball)
-	if x <= 0 or a >= width:
-		speedx = -speedx
-	if y <= 0:
-		speedy = -speedy
-	elif b >= 600:
-		c = (x + a) / 2
-		(p, q, r, s) = canvas.coords(platform)
-		ballpos = canvas.coords(ball)
-		badzone = canvas.coords(nogozone)
-		if (p <= c <= r):
-			speedx = speedx + random.randint(0, 2)
-			speedy = -speedy - random.randint(0, 2)
-			score += 10
-			txt = 'Score: ' + str(score)
-			canvas.itemconfigure(scoreText, text = txt)
-		elif overlapping(ballpos, badzone):
-			canvas.create_image(640, 360, image = gameover)
-			return
-	canvas.move(ball, speedx, speedy)
+	global speedx, speedy, score, pause
+	if pause is False:
+		(x, y, a, b) = canvas.coords(ball)
+		if x <= 0 or a >= width:
+			speedx = -speedx
+		if y <= 0:
+			speedy = -speedy
+		elif b >= 600:
+			c = (x + a) / 2
+			(p, q, r, s) = canvas.coords(platform)
+			ballpos = canvas.coords(ball)
+			badzone = canvas.coords(nogozone)
+			if (p <= c <= r):
+				speedx = speedx + random.randint(0, 2)
+				speedy = -speedy - random.randint(0, 2)
+				score += 10
+				txt = 'Score: ' + str(score)
+				canvas.itemconfigure(scoreText, text = txt)
+			elif overlapping(ballpos, badzone):
+				canvas.create_image(640, 360, image = gameover)
+				return
+		canvas.move(ball, speedx, speedy)
 	canvas.after(20, moveball)
 
 def moveplatform():
 	canvas.pack()
+
+def setbosskey(w, h):
+    bosskeywindow = Toplevel()
+    bosskeywindow.title("Document1")
+    ws = window.winfo_screenwidth()  # computer's screen size
+    hs = window.winfo_screenheight()
+    x = (ws/2) - (w/2)  # calculate center
+    y = (hs/2) - (h/2)
+    bosskeywindow.geometry('%dx%d+%d+%d' % (w, h, x, y))  # bosskey size
+    return bosskeywindow
+
+def createstars():
+    for i in range(400):
+        x = random.randint(1, 1279)
+        y = random.randint(1, 350)
+        size = random.randint(2, 5)
+        f = random.randint(0, 2)
+        xy = (x, y, x + size, y + size)
+        tmp_star = canvas.create_oval(xy, fill=c[f])
+        star.append(tmp_star)
 
 def setwindowdimensions(w, h):
     window = Tk()
@@ -75,8 +117,24 @@ score = 0
 txt = 'Score: ' + str(score)
 scoreText = canvas.create_text(width / 2, 650, fill = 'white', font = 'Arial 20 italic bold', text = txt)
 
+pause = False
+pauseit = Button(window, text = 'Play/Pause', command = pausegame, anchor = 'n')
+pauseit.configure(activebackground = 'white')
+pausethis = canvas.create_window(1182, 23, anchor = 'nw', window = pauseit)
+
+bosskeywindow = setbosskey(width, height)
+boss = Canvas(bosskeywindow, bg = 'black', width = width, height = height)
+boss.pack()
+loremipsum = PhotoImage(file = "loremipsumtextscaled.png")
+document = boss.create_image(640, 360, image = loremipsum)
+bosskeywindow.withdraw()
+bossflag = False
+
 canvas.bind('<Left>', leftkey)
 canvas.bind('<Right>', rightkey)
+canvas.bind('<Tab>', bosskey)
+bosskeywindow.bind('<Tab>', bosskey)
+
 canvas.focus_set()
 
 direction = 'right'
